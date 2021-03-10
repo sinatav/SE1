b99b9ac475005415a967f682c62f0b87ca183514
ellykhodaie <elahekhodaie@yahoo.com>
2021-03-10 18:59:55 +0330

    csv module created

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 from utils.connections import connect, get_decoded_data
-
+from utils.features import save_csv
 base_url = 'https://search.codal.ir/api/search/v1/'
 tag = 'companies'
 url = base_url + tag
@@ -12,3 +12,5 @@ data = get_decoded_data(page)
 
 for i in data:
     print(i)
+
+save_csv(data, "test")
