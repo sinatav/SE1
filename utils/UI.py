5bdfb52122308f7aaabbf45988ea5eb91015e7b8
Sina <stavakkoli1999@gmail.com>
2021-03-10 17:54:03 +0330

    add show data to ui

--- utils/UI.py
+++ utils/UI.py
@@ -1,5 +1,6 @@
 from utils.dataCleaning import *
 from utils.connections import *
+from utils.features import *
 
 url = 'https://search.codal.ir/api/search/v1/companies'
 
@@ -8,7 +9,10 @@ if con == 1:
     print("data is crawling from source destination:")
     data = get_decoded_data(connect(url))
     print("data is saved in a dict form")
+    op1 = int(input("wanna see data?\n1. yes\t2. no\n"))
+    if op1 == 1:
+        show_data_raw(data)
 else:
-    exit()
+    exit("BYEEEEE!")
 
 
