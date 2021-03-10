97c34a78eaca3cb4b4a509f69e4e7dc5b09718b0
Sina <stavakkoli1999@gmail.com>
2021-03-10 18:07:39 +0330

    show in dictionary module added (+in ui)

--- utils/UI.py
+++ utils/UI.py
@@ -9,9 +9,12 @@ if con == 1:
     print("data is crawling from source destination:")
     data = get_decoded_data(connect(url))
     print("data is saved in a dict form")
-    op1 = int(input("wanna see raw data?\n1. yes\t2. no\n"))
+    op1 = int(input("wanna see raw data or in a dict form?\n1. raw\t2. dict\n"))
     if op1 == 1:
         show_data_raw(data)
+    elif op1 == 2:
+        show_dict_data(data)
+
 else:
     exit("BYEEEEE!")
 
